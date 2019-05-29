7eaa594c8f9491d5f3e70e99896725815388cfcc
sjning <ningshijie@meituan.com>
2019-05-29 17:02:43 +0800

    修改广告 升级版本号

--- main/src/main/java/com/old/interesting/music/activities/AdsBaseActivity.java
+++ main/src/main/java/com/old/interesting/music/activities/AdsBaseActivity.java
@@ -8,12 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.reward.RewardItem;
-import com.google.android.gms.ads.reward.RewardedVideoAd;
-import com.google.android.gms.ads.reward.RewardedVideoAdListener;
-import com.old.interesting.music.BuildConfig;
 import com.old.interesting.music.Config;
 import com.old.interesting.music.R;
 import com.old.interesting.music.fragments.PlayerFragment.PlayerFragment;
@@ -28,114 +23,11 @@ import newui.base.BaseActivity;
  * description:
  */
 public class AdsBaseActivity extends BaseActivity {
-    final String AD_UNIT_ID_DEBUG = "ca-app-pub-3940256099942544/5224354917";
-
-    final String AD_UNIT_ID = "ca-app-pub-7203590267874431/7784247488";
-
-    protected RewardedVideoAd mRewardedVideoAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initAds();
-        Config.PLAY_ADS_COUNT = 0;
     }
-
-    @Override
-    protected void onResume() {
-        mRewardedVideoAd.resume(this);
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        mRewardedVideoAd.pause(this);
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        mRewardedVideoAd.destroy(this);
-        super.onDestroy();
-    }
-
-    private void initAds() {
-        MobileAds.initialize(this, CommonUtils.getMetaData(this, "ads_id"));
-        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
-        mRewardedVideoAd.setRewardedVideoAdListener(new RewardedVideoAdListener() {
-            @Override
-            public void onRewardedVideoAdLoaded() {
-
-            }
-
-            @Override
-            public void onRewardedVideoAdOpened() {
-
-            }
-
-            @Override
-            public void onRewardedVideoStarted() {
-
-            }
-
-            @Override
-            public void onRewardedVideoAdClosed() {
-                // Load the next rewarded video ad.
-                loadRewardedVideoAd();
-            }
-
-            @Override
-            public void onRewarded(RewardItem rewardItem) {
-
-            }
-
-            @Override
-            public void onRewardedVideoAdLeftApplication() {
-
-            }
-
-            @Override
-            public void onRewardedVideoAdFailedToLoad(int i) {
-
-            }
-
-            @Override
-            public void onRewardedVideoCompleted() {
-
-            }
-        });
-        loadRewardedVideoAd();
-    }
-
-    protected void loadRewardedVideoAd() {
-        if(BuildConfig.DEBUG){
-            mRewardedVideoAd.loadAd(AD_UNIT_ID_DEBUG, new AdRequest.Builder().build());
-        }else {
-            mRewardedVideoAd.loadAd(AD_UNIT_ID, new AdRequest.Builder().build());
-        }
-
-    }
-
-    public void showAd() {
-        count++;
-        if (count % Config.COUNT != 0) {
-            return;
-        }
-        if(Config.PLAY_ADS_COUNT == Config.MAX_PLAY_COUNT){
-            return;
-        }
-        showRewardAd();
-    }
-
-    private void showRewardAd(){
-        if (mRewardedVideoAd != null && mRewardedVideoAd.isLoaded()) {
-            mRewardedVideoAd.show();
-            Config.PLAY_ADS_COUNT++;
-        }
-    }
-
-
-    protected int count = 1;
 
     protected void showStarDialog(){
         new  AlertDialog.Builder(this)
