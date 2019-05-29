c7af65fb7c9e8ee1d58e61e54eca9db5554acd66
sjning <ningshijie@meituan.com>
2019-05-29 17:02:43 +0800

    1、升级版本号 2、修改广告展示

--- main/src/main/java/com/old/interesting/music/activities/AdsInserterActivity.java
+++ main/src/main/java/com/old/interesting/music/activities/AdsInserterActivity.java
@@ -2,13 +2,14 @@ package com.old.interesting.music.activities;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.old.interesting.music.BuildConfig;
 import com.old.interesting.music.Config;
+import com.old.interesting.music.utilities.CommonUtils;
 
 /**
  * Created by sjning
@@ -17,15 +18,21 @@ import com.old.interesting.music.Config;
  */
 public class AdsInserterActivity extends AdsBaseActivity {
     private InterstitialAd mInterstitialAd;
+    protected int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initAds();
+    }
+
+    private void initAds() {
+        MobileAds.initialize(this, CommonUtils.getMetaData(this, "ads_id"));
+        Config.PLAY_ADS_COUNT = 0;
         mInterstitialAd = new InterstitialAd(this);
         mInterstitialAd.setAdUnitId(BuildConfig.DEBUG ? "ca-app-pub-3940256099942544/1033173712"
-                : "ca-app-pub-8270196426610526/8484473721");
+                : "ca-app-pub-7203590267874431/5624153859");
         mInterstitialAd.loadAd(new AdRequest.Builder().build());
-
         mInterstitialAd.setAdListener(new AdListener() {
             @Override
             public void onAdClosed() {
@@ -46,7 +53,6 @@ public class AdsInserterActivity extends AdsBaseActivity {
         }
     }
 
-    @Override
     public void showAd() {
         count++;
         if (count % Config.COUNT != 0) {
